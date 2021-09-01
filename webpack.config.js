7a9caf2c07aaf29bbd92b19c855494c140834023
SpidySamurai <javierchiortiz@gmail.com>
2021-09-01 01:11:34 -0500

    Fix html plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,10 @@ module.exports = {
     ],
   },
   plugins: [
-    new htmlWebpackPlugin([
-      {
-        inject: true,
-        template: "./public/index.html",
-        filename: "./index.html",
-      },
-    ]),
+    new htmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
   ],
 };
